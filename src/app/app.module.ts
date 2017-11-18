2ce7339e5e10a798cb534aee250acb7535b8b7da
Le Van Sang <vansang1706@gmail.com>
2017-11-18 15:37:59 +0700

    Completed pass 7: Routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,8 @@ import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { HeroService } from './hero.service';
 import { MessagesComponent } from './messages/messages.component';
 import { MessageService } from './message.service';
+import { AppRoutingModule } from './/app-routing.module';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 
 
@@ -16,11 +18,13 @@ import { MessageService } from './message.service';
     AppComponent,
     HeroesComponent,
     HeroDetailComponent,
-    MessagesComponent
+    MessagesComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    AppRoutingModule
   ],
   providers: [
     HeroService,
--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -11,11 +11,17 @@ export class HeroService {
 
   constructor(private messageService: MessageService) { }
 
-  getHeroes(): Observable<Hero[]>{
+  getHeroes(): Observable<Hero[]> {
     // Todo: send the message _after_ fetching the heroes
     this.messageService.add("HeroService: fetched heroes");
 
     return of(HEROES);
+  }
+
+  getHero(id: number): Observable<Hero> {
+    // Todo: send the message _after_ fetching the hero
+    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    return of(HEROES.find(hero => hero.id === id));
   }
 
 }
